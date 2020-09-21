ed76a675412529c4507f960ded4f64e94dd9be3c
bmichas <michasbartek@gmail.com>
2020-09-21 19:29:46 +0200

    test

--- data_gen.py
+++ data_gen.py
@@ -1,102 +1,25 @@
-import random
-
-
-def data_gen(gate_type, lines, gen_type):
-    # listy do generatora
-    row_f_col = []
-    row_s_col = []
-    # tablica do zmiany liczby na bramke
-    gate_name = {'1': 'AND', '2': 'OR', '3': 'NOT',
-                 '4': 'NAND', '5': 'NOR', '6': 'XOR', '7': 'XNOR'}
-    gate_type = str(gate_type)
-    fin_gate_name = gate_name[gate_type]
-    print(fin_gate_name)
-    if gen_type == 1:
-        # generowanie list
-        for i in range(0, lines):
-            f_col = random.randint(0, 1)
-            s_col = random.randint(0, 1)
-            row_f_col.append(f_col)
-            row_s_col.append(s_col)
-        print(row_f_col, row_s_col)
-
-    else:
-        print("koniec")
-
-
-def mat_function():
-    return 0
-
-
-def logic_gates():
-    print("Wpisz ilość wierszy do wygenerowania: ")
-    rows = valInput(1)
-    print("Wybierz rodzaj bramki logicznej: ")
-    print("1. AND")
-    print("2. OR")
-    print("3. NOT")
-    print("4. NAND")
-    print("5. NOR")
-    print("6. XOR")
-    print("7. XNOR")
-    print("0. Cofnij")
-    gate = valInput(2)
-    data_gen(gate, rows, 1)
-
-
 def valInput(CheckType):
     pick = input("Wybierz co chesz zrobić: ")
-    gates = [1, 2, 3, 4, 5, 6, 7]
-    # Wybierranie co chcesz zrobic czy matematyczne czy bramka
     if CheckType == 0:
         while True:
             if pick.isdigit() == False:
                 pick = input("Wprowadziałeś znak: ")
                 continue
-            elif 0 <= int(pick) < 3:
+            elif 0 < int(pick) < 3:
                 pick = int(pick)
                 break
             else:
                 pick = input("Liczba z poza zakresu: ")
                 continue
-    # Wybieranie ilości wierszy
-    elif CheckType == 1:
-        while True:
-            if pick.isdigit() == False:
-                pick = input("Wprowadziłeś znak: ")
-                continue
-            elif int(pick) > 1000:
-                pick = input("Wprowadziłes za duża liczbe: ")
-                continue
-            elif int(pick) < 1001:
-                pick = int(pick)
-                break
-    # Wybieranie bramki
-    elif CheckType == 2:
-        while True:
-            if pick.isdigit() == False:
-                pick = input("Wprowadziałeś znak: ")
-                continue
-            elif int(pick) in gates:
-                pick = int(pick)
-                break
-            else:
-                main()
-    return pick
+        return pick
 
 
 def main():
     print("Generuj dane: ")
-    print("1. Bramki logiczne")
-    print("2. Rownania matematyczne")
-    print("0. Wyjdź z programu")
+    print("1. Bramki logiczne: ")
+    print("2. Rownania matematyczne: ")
     x = valInput(0)
-    if x == 1:
-        logic_gates()
-    elif x == 2:
-        mat_function()
-    else:
-        print("Do zobaczenia!")
+    print(x)
 
 
 if __name__ == "__main__":
